88adf941f81bb5a24e67c572dbcd636a670ae606
@chenlianghui <@769514079@qq.com>
2017-06-20 17:34:57 +0800

    Initial Commit

---
+++ ZQHousekeep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZQHousekeep
+//
+//  Created by 陈良辉 on 2017/6/20.
+//  Copyright © 2017年 礼邻网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZQHousekeep/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZQHousekeep
+//
+//  Created by 陈良辉 on 2017/6/20.
+//  Copyright © 2017年 礼邻网络. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
